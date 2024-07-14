f7b07144715c6dc0d8ba704b1f627f82c37f26d6
Leon Gieser <leon.gieser@gmail.com>
2024-07-14 15:54:58 +0200

    feat(i18n): i18n setup with next-intl

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+//https://next-intl-docs.vercel.app/docs/getting-started/app-router/with-i18n-routing#move-i18n-ts
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
